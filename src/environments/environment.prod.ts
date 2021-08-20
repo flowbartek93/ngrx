2ee6d3e32eed2c0ce5e327ede664526ceb6edd70
Bartek <submission93@gmail.com>
2021-08-20 09:35:15 +0200

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  firebaseAPIKey: 'AIzaSyA9jzaHPNIxCR7ZxwLz-29XBirFIJ89Qfo',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  firebaseAPIKey: 'AIzaSyA9jzaHPNIxCR7ZxwLz-29XBirFIJ89Qfo',
+};
